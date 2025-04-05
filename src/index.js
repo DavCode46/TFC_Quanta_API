a8089180fc7b0a6fe7d1c5d3c1cd9503cce469d7
Davidmb <davidblanco1993@gmail.com>
2025-04-05 21:10:34 +0200

    feat: register route done and  DB connection configured

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import 'dotenv/config'
-import express from 'express'
+import 'dotenv/config';
+import express from 'express';
+import connect from 'mongoose';
+
+import userRoutes from './routes/user.routes.js';
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+app.use('/api/users', userRoutes)
+
+connect(process.env.MONGODB_URI)
+  .then(
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  ).catch((error) => console.error(error))
